2165eda4c54391ce559998085a49e566e4911a11
Kushal-np <wrckushal@gmail.com>
2025-08-13 18:58:48 +0545

    Added index.js in the backend

---
+++ client/src/components/button/index.jsx
@@ -0,0 +1,11 @@
+function Button(){
+    return (
+        <div className="border-2 border-solid border-black  bg-black text-white p-2 hover:text-black hover:bg-white hover:border-white transition-all duration-100 inline-block">
+            Click here
+        </div>
+        
+    )
+    
+}
+
+export default Button;
